ba98af8b9f25c1b6d9565d7ae36f7f6bacc63cd5
ekram-web <unuse2001@gmail.com>
2025-04-12 12:21:51 -0700

    react based apple clone home page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Alert from "./components/Alert/Alert";
+import First from "./components/Firtstsection/First";
+import Second from "./components/Secondsection/Second";
+import Thrid from "./components/Thirdsection/Thrid";
+import Four from "./components/Fourthsection/Four";
+import Five from "./components/Fifthsection/Five";
+import Six from "./components/Sixthsection/Six";
+function App() {
+  return (
+    <>
+      <Header />
+      <Alert />
+      <First />
+      <Second />
+      <Thrid />
+      <Four />
+      <Five />
+      <Six />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
